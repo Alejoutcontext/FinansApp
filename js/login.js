8927d117021234b69789a64923c94cd7266f4c4d
Alejandro <alejandroherrera0227@gmail.com>
2026-02-05 12:22:16 -0500

    Login

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+toggle.addEventListener("click", () => {
+  const type = password.type === "password" ? "text" : "password";
+  password.type = type;
+});
